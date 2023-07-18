6eddcc050101a363ee2c0eeebdaff9db2c68223f
aravion1 <aravion1@gmail.com>
2023-07-18 11:47:28 +0700

    Добавил получение токена с Bearer

--- pkg/handler/middleware.go
+++ pkg/handler/middleware.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -10,9 +13,29 @@ import (
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		t := time.Now()
+		_, err := getBearer(c)
+
+		if err != nil {
+			newErrorResponse(c, http.StatusUnauthorized, err.Error())
+			return
+		}
 
 		c.Next()
 
 		fmt.Println(time.Since(t))
 	}
+}
+
+func getBearer(c *gin.Context) (string, error) {
+	bearer := c.GetHeader("Authorization")
+	if bearer == "" {
+		return "", errors.New("authentication failed")
+	}
+
+	bearer_slice := strings.Split(bearer, " ")
+	if len(bearer_slice) < 2 {
+		return "", errors.New("authentication failed")
+	}
+
+	return bearer_slice[1], nil
 }
